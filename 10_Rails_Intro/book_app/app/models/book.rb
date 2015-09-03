f52a72918c7c58782093560294045146fc97a3f6
Leo Starlord <leonidtsh@gmail.com>
2015-09-02 18:55:54 -0700

    class_rails

---
+++ 10_Rails_Intro/book_app/app/models/book.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id             :integer          not null, primary key
+#  title          :string
+#  author         :string
+#  year_published :integer
+#  genre          :string
+#  available      :boolean
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#
+
+class Book < ActiveRecord::Base
+end
